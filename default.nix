01dab2d1882b5b8e1112f49947292e16ea4af4ef
GGG <gggkiller2@gmail.com>
2025-01-28 09:43:22 -0300

    Update dependencies.

--- default.nix
+++ default.nix
@@ -1,10 +1,9 @@
 {
-  lib,
   buildDotnetModule,
   dotnetCorePackages,
   ffmpeg,
 }:
-buildDotnetModule rec {
+buildDotnetModule {
   pname = "m3u8-dl";
   version = "0.1";
 
@@ -13,12 +12,13 @@ buildDotnetModule rec {
   projectFile = "src/m3u8-dl.csproj";
   testProjectFile = "test/m3u8-dl.Test.csproj";
   # To update nuget deps, run `$(nix-build -A fetch-deps ./build.nix --no-out-link)`
-  nugetDeps = ./deps.nix;
+  nugetDeps = ./deps.json;
 
-  runtimeDeps = [ffmpeg];
+  runtimeDeps = [ ffmpeg ];
 
   dotnet-sdk = dotnetCorePackages.sdk_8_0;
   dotnet-runtime = dotnetCorePackages.runtime_8_0;
 
-  executables = ["m3u8-dl"];
+  executables = [ "m3u8-dl" ];
+  meta.mainProgram = "m3u8-dl";
 }
