fc643fb56fc9943d9bc0e5f00dd94c87241ac061
GGG <gggkiller2@gmail.com>
2025-01-28 10:31:33 -0300

    Use fixed ffmpeg version and remove test project.

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 {
   buildDotnetModule,
   dotnetCorePackages,
-  ffmpeg,
+  ffmpeg_7,
 }:
 buildDotnetModule {
   pname = "m3u8-dl";
@@ -10,11 +10,10 @@ buildDotnetModule {
   src = ./.;
 
   projectFile = "src/m3u8-dl.csproj";
-  testProjectFile = "test/m3u8-dl.Test.csproj";
   # To update nuget deps, run `$(nix-build -A fetch-deps ./build.nix --no-out-link)`
   nugetDeps = ./deps.json;
 
-  runtimeDeps = [ ffmpeg ];
+  runtimeDeps = [ ffmpeg_7 ];
 
   dotnet-sdk = dotnetCorePackages.sdk_9_0;
   dotnet-runtime = dotnetCorePackages.runtime_9_0;
